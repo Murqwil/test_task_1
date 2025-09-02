abae491393dbeb3de666cddeea6c30b4bd2a66cf
Timur <sabirzianov,timur@yandex.ru>
2025-09-02 18:47:19 +0300

    dev Осталось реализовать README

--- src/main/java/ru/test/solution/exception/WalletNotFoundException.java
+++ src/main/java/ru/test/solution/exception/WalletNotFoundException.java
@@ -1,7 +1,13 @@
 package ru.test.solution.exception;
 
+import java.util.UUID;
+
 public class WalletNotFoundException extends RuntimeException {
   public WalletNotFoundException(String message) {
     super(message);
+  }
+
+  public WalletNotFoundException(UUID walletId) {
+    super("Wallet not found with id: " + walletId);
   }
 }
